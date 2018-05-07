348930b01b40a849b613ce570fdf28a8ea644c33
jlarge1 <largejonathan@gmail.com>
2018-05-06 22:44:11 -0400

    it works!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,10 +12,12 @@ var words = [
     "vine", "palm", "chlorophyll", "oxygen", "carbon dioxide", "terra cotta"
 ];
 var letters = [];
+var correct = [];
 var wrong = [];
 var match = [];
-//var wordChoice = Math.floor(Math.random() * words.length);
-var wordChoice = 9;
+var letterNum = [];
+var wordChoice = Math.floor(Math.random() * words.length);
+//var wordChoice = 9;
 var word = words[wordChoice];
 
 
@@ -25,6 +27,7 @@ function addGuess(x) {
     letters.push(x);
     docGuesses.textContent = letters;
     wrongLetter();
+    return letters;
 }
 
 function alreadyThere(x) {
@@ -34,20 +37,52 @@ function alreadyThere(x) {
 function wrongLetter() {
     wrong.push("x");
     var y = wrong.length;
-    docWrongGuess.textContent = y + "/10 wrong guesses"
-    if (y >= 10) {
+    docWrongGuess.textContent = y + "/5 wrong guesses"
+    if (y >= 5) {
         endGame();
     }
 }
 
 function endGame() {
     docConversation.textContent = "You lost :(";
+    revealWord();
+}
+
+function revealWord() {
+    for (i = 0; i < word.length; i++) {
+        if (word[i] == " ") {
+            var letter = document.createElement("div");
+            letter.setAttribute("Id", "letter");
+            docWord.appendChild(letter);
+        }
+        else {
+            var z = word[i];
+            var y = document.getElementById("letter" + i);
+            y.textContent = z;
+        }
+    }
 }
 
 function checkDup(x) {
     if (letters.length > 0) {
         for (i = 0; i < letters.length; i++) {
             if (x == letters[i]) {
+                return true;
+            } else {
+                continue;
+            }
+        }
+        return false;
+    }
+    else {
+        return false;
+    }
+}
+
+function checkCorrect(x) {
+    if (correct.length > 0) {
+        for (i = 0; i < correct.length; i++) {
+            if (x == correct[i]) {
                 return true;
             } else {
                 continue;
@@ -76,37 +111,64 @@ function initializeGame(word) {
             docWord.appendChild(letter);
         }
     }
-    docWrongGuess.textContent = "0/10 wrong guesses";
+    docWrongGuess.textContent = "0/5 wrong guesses";
     return word;
 }
 
-function keyCheck(x){
+function keyCheck(x) {
     var z = x.keyCode;
-    if ((z >= 65) && (z <= 90)){
+    if ((z >= 65) && (z <= 90)) {
         return true;
     }
     else {
         docConversation.textContent = "Guess the word! Type a LETTER."
-        return false;        
-    } 
+        return false;
+    }
 }
 
 function checkGuess(x) {
     for (i = 0; i < word.length; i++) {
-        if (x == word[i]){
+        if (x == word[i]) {
             match.push(i);
         } else {
             continue;
         }
     }
-    for (i = 0; i < match.length; i++) {
-        var z = match[i]
-        var y = document.getElementById("letter" + z);
-        y.textContent = x;
-    } 
-    addGuess(x);
-    match = [];
+    return match;
+}
 
+function appendMatch(x) {
+    if (match.length > 0) {
+        for (i = 0; i < match.length; i++) {
+            var z = match[i]
+            var y = document.getElementById("letter" + z);
+            y.textContent = x;
+            correct.push(x);
+        }
+    } else {
+        addGuess(x);
+    }
+    match = [];
+    return correct;
+}
+
+function checkEnd(x) {
+    if (correct.length == letterNum.length) {
+        docConversation.textContent = "YOU WON!";
+        docGuesses.textContent = "";
+        docWrongGuess.textContent = "";
+    }
+}
+
+function numberLetters(x) {
+    for (i = 0; i < x.length; i++) {
+        if (x[i] == " ") {
+            continue;
+        } else {
+            letterNum.push(i);
+        }
+    }
+    return letterNum;
 }
 
 function main(x) {
@@ -115,9 +177,15 @@ function main(x) {
     if (keyCheck(x)) {
         if (checkDup(y)) {
             alreadyThere(y);
+        } else if (checkCorrect(y)) {
+            alreadyThere(y);
         } else {
             checkGuess(y);
+            appendMatch(y);
+            checkEnd(y);
         }
+        console.log("correct length: " + correct.length);
+        console.log("letterNum length: " + letterNum.length);
     }
 }
 
@@ -127,4 +195,6 @@ function main(x) {
 //==========================================================================
 document.addEventListener("keyup", function () { main(event); });
 docConversation.textContent = message1;
+numberLetters(word);
 initializeGame(word);
+
